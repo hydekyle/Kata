b814f5e9c4a2b553ad3f936c1a1c4b1e030fb3dd
Ayoze M <hydekyle@hotmail.com>
2019-10-04 19:46:41 +0100

    Stage 2 completado

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,27 @@
-function FizzBuzz(){
-  for (var x = 0; x <= 100; x++) console.log(CheckNumber(x));
+function FizzBuzz() {
+  for (var x = 0; x <= 100; x++) {
+    console.log(CheckNumber(x));
+  }
 }
 
-function CheckNumber(number){
-  if (number % 3 == 0) return "Fizz";
-  if (number % 5 == 0) return "Buzz";
+function CheckNumber(number) {
+  if (number % 3 == 0 && ContainThisNumber(number, 3)) {
+    return "Fizz";
+  }
+  if (number % 5 == 0 && ContainThisNumber(number, 5)) {
+    return "Buzz";
+  }
   return number;
+}
+
+function ContainThisNumber(number, n) {
+  let numberString = number.toString();
+  for (var x = 0; x < numberString.length; x++){
+    if (numberString[x] == n) {
+      return true;
+    }
+  }
+  return false;
 }
 
 FizzBuzz();
